77d8682cc22bf5fa4575efb98d722cb78101a45c
mbron770 <mbronfin.tsd@gmail.com>
2023-07-06 15:41:01 -0400

    a

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word1): 
+        self.word1_sorted = sorted([letter for letter in word1])
+        
+    def match(self, word_list): 
+        return [word2 for word2 in word_list if sorted([letter for letter in word2]) == self.word1_sorted]
+    
+
+  
+        
+    
+    
+    
+
+
+
+
+
+
+
+
